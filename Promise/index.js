965046aef877da743dd1fbc46cf45732b9ee83a2
Ash Lai <z0975201508@gmail.com>
2021-08-30 21:29:39 +0800

    補充:非同步問題

--- Promise/index.js
+++ Promise/index.js
@@ -1,8 +1,13 @@
+// 關於 Promise 常見問題
+
 // Q: Promise vs Ajax ?
 // A: Ajax 可以向伺服器傳送及取得資料，過程不需要重新整理瀏覽器頁面，可大幅提升使用者體驗且減少伺服器負擔
 
 // Promise vs Async、Await ?
 // Promise 是用來優化非同步的語法，而 Async、Await 是基於 Promise 讓非同步的語法結構類似於 "同步語言" 的語法糖，使開發上易於管理與解讀
+
+
+// 非同步的問題
 
 // example 1:
 
@@ -31,3 +36,4 @@ console.log(data); // {}
 // A: data仍為空物件，因為console.log的順序會再axios實際取得資料並指向data之前
 // 換言之console.log並沒有等到資料回來就印出來，此時data仍為空物件 {}
 
+// Promise 的結構及狀態
